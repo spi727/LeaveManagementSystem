77b51fcb5fe5c0427b792e2fc70b576cacec4534
Praveen Sagar <praveen975427@gmail.com>
2025-06-16 14:10:16 +0530

    Updated the File Load Service logic

--- Services/FileLoadService.cs
+++ Services/FileLoadService.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using LeaveManagementSystem.Models;
+
+namespace LeaveManagementSystem.Services
+{
+    public class FileLoadService
+    {
+        private readonly string filePath = "leave_records.json";
+
+        public List<LeaveApplication> LoadLeaves()
+        {
+            if (!File.Exists(filePath))
+                return new List<LeaveApplication>();
+
+            string json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<List<LeaveApplication>>(json) ?? new List<LeaveApplication>();
+        }
+    }
+}
