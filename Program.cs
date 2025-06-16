f3cdc2a498ff31d28ad0ac0c6c2fc0253e891d56
Praveen Sagar <praveen975427@gmail.com>
2025-06-16 10:51:36 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LeaveManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
