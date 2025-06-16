a3254d80f80e6f4a20f4bdcb74b9a637e2ff2ff0
Pritish Priyadasan <99426389+Pritish50@users.noreply.github.com>
2025-06-16 14:45:15 +0530

    i create both leave request and service file

---
+++ Models/LeaveRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaveManagementSystem.Models
+{
+    public class LeaveRequest
+    {
+        public string EmployeeId { get; set; }
+        public string ManagerId { get; set; }
+        public DateTime StartDate { get; set; }
+        public string Status { get; set; }
+    }
+}
