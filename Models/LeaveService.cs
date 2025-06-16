cf5a6ca7c0d42b30e8d4677a60beee1b4078e028
Pritish Priyadasan <99426389+Pritish50@users.noreply.github.com>
2025-06-16 14:37:00 +0530

    i create a leaveService .cs file and execute my code

---
+++ Models/LeaveService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaveManagementSystem.Models
+{
+    public class LeaveService
+    {
+        private List<LeaveRequest> _leaveRequests;
+        public LeaveService(List<LeaveRequest> requests)
+        {
+            _leaveRequests = requests;
+        }
+
+        public List<LeaveRequest> GetLeaveHistoryByEmployee(string employeeId)
+        {
+            return _leaveRequests
+                .Where(lr => lr.EmployeeId == employeeId)
+                .OrderByDescending(lr => lr.StartDate)
+                .ToList();
+        }
+        public List<LeaveRequest> GetPendingApprovala(string managerId)
+        {
+            return _leaveRequests
+                .Where(lr => lr.ManagerId == managerId && lr.Status == "Pending")
+                .OrderBy(lr => lr.StartDate)
+                .ToList();
+        }
+    }
+}
